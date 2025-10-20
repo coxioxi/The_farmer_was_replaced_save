9c36a3b02dd40560d96baa1d8cc0d1f7e4b809c6
Samuel Costa <samukaka1000@gmail.com>
2025-10-19 20:11:54 -0400

    Setting pumpkin plant

--- Plantation.py
+++ Plantation.py
@@ -1,3 +1,5 @@
+import Reset_position
+
 def getWood():
 	if get_pos_y() % 2 != 0:
 		plant(Entities.Tree)
@@ -14,14 +16,25 @@ def getCarrot():
 	plant(Entities.Carrot)
 		
 def getPumpkin():
-	for i in range(get_world_size()):
-		for j in range(get_world_size()):
-			if get_ground_type() != Grounds.Soil:
-				till()
-			plant(Entities.Pumpkin)
-			
+	alive_pump = False
 	
-
+	for i in range(2):
+		for j in range(get_world_size()):
+			for k in range(get_world_size()):
+				if get_ground_type() != Grounds.Soil:
+					till()
+				if i % 2 == 0:
+					move(North)
+				else:
+					move(South)
+				if not can_harvest():
+					plant(Entities.Pumpkin)
+					
+			if i == get_world_size()-1:
+				Reset_position.main()
+			else:
+				move(East)
+					
 def waterAnal():
 	if get_water() <= 0.5:
 		use_item(Items.Water)
--- Reset_position.py
+++ Reset_position.py
@@ -1,4 +1,9 @@
-while get_pos_y() != 0:
-	move(South)
-while get_pos_x() != 0:
-	move(West)
+def main():
+	def reset_pos():
+		while get_pos_y() != 0:
+			move(South)
+		while get_pos_x() != 0:
+			move(West)
+
+if __name__ == "__main__":
+	main()
